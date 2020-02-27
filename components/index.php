e30d650c31a37d5589ba875aa41e781d2df4644a
Conea Sorin ( SPIKECSI -TEAM ) <coneasorin@outlook.com>
2020-02-27 15:42:15 +0200

    Git1
    Licenta prof Softescu

---
+++ components/index.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'sidebar.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+
+  <title>asd</title>
+</head>
+<body>
+<?php
+sidebar();
+?>
+</body>
+</html>
